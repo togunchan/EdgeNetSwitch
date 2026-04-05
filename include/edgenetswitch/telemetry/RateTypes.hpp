bf6d6fcc44d6453dfcf7e6c49e0c01852835257d
togunchan <togunchan@gmail.com>
2026-04-05 15:12:23 +0300

    feat(telemetry): add rate smoothing value types

---
+++ include/edgenetswitch/telemetry/RateTypes.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+
+namespace edgenetswitch
+{
+    struct RateSmootherConfig
+    {
+        double alpha{0.2};
+        std::uint64_t window_ms{1000};
+    };
+
+    struct RateSnapshot
+    {
+        bool valid{false};
+        std::uint64_t raw_per_sec{0};
+        std::uint64_t smoothed_per_sec{0};
+    };
+
+} // namespace edgenetswitch
